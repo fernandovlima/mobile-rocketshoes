a374a7087e661e41ba9db16c9d8d2b1692654cb3
fernandovlima <fernandovlima.s@gmail.com>
2019-11-06 01:08:39 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StatusBar, Text, SafeAreaView} from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <SafeAreaView>
+        <StatusBar barStyle="light-content" backgroundColor="#45B39D" />
+        <Text>ROCKET SHOES MOBILE</Text>
+      </SafeAreaView>
+    </>
+  );
+}
