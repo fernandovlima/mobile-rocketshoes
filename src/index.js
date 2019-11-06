e782669eea8dfc19f7fbc2068833b52f519dad78
fernandovlima <fernandovlima.s@gmail.com>
2019-11-06 01:14:30 -0300

    add eslint, prettier

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StatusBar, Text, SafeAreaView} from 'react-native';
+import { StatusBar, Text, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
